9c9a7cc8e20f27b0bf924fc97e3c744b84edd0d9
DigioSSilva <diegodossantos788@gmail.com>
2024-10-30 16:11:34 -0300

    dotenv config add

--- app/settings.py
+++ app/settings.py
@@ -22,7 +22,7 @@ import os
 locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
